d86f9ed60314c34386b14ab51dd072d99544b7b2
baiqyustiarini <baiqyustiarini@gmail.com>
2022-12-07 17:03:15 +0700

    centering div

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -158,18 +158,18 @@ const HomeScreen = () => {
       </section> */}
 
       <section id="hero" className="w-full">
-        <div className="absolute z-10 lg:mt-20 container mx-8 lg:mx-[183px]">
+        <div className="absolute z-10 lg:mt-20 container left-1/2 transform -translate-x-1/2">
           <div className="mx-auto w-full flex flex-col lg:flex-row">
-            <div className="w-full lg:w-1/2 flex flex-col justify-center  items-center text-white">
+            <div className="w-full md:mt-24 lg:w-1/2 flex flex-col justify-center  items-center text-white">
               <div className="lg:px-5 flex flex-col justify-center items-center bg-black opacity-70 rounded-2xl lg:items-start text-center lg:text-left">
-                <h1 className="py-10  text-2xl sm:text-5xl font-light tracking-wide leading-tight">
-                  Arsitektur, <br />
+                <h1 className="py-10 md:py-4 text-2xl sm:text-5xl font-light tracking-wide leading-tight">
+                  Arsitektur, <br className="md:hidden" />
                   Bangunan, <br />
                   Interior kantor, hunian, dan ruko
                 </h1>
               </div>
               <div className="my-4 bg-black w-full opacity-70 rounded-full">
-                <p className="text-2xl text-center font-bold text-white tracking-wide">
+                <p className="text-2xl  text-center font-bold text-white tracking-wide">
                   PT. Abiudaya Raya Utama
                 </p>
               </div>
@@ -227,10 +227,10 @@ const HomeScreen = () => {
               </div>
             </div>
           </div>
-          <div className="lg:mt-8 md:mt-0 md:p-0 mb-24 bg-[#e3dac4] rounded-lg p-5 w-full md:h-24 grid grid-cols-12 sm:grid-cols-4 gap-2 text-xs md:text-sm">
+          <div className="lg:mt-4 lg:h-auto mb-24 bg-[#e3dac4] rounded-lg p-5 w-full md:h-24 grid grid-cols-12 sm:grid-cols-4 gap-2 text-xs md:text-sm">
             <div
               onClick={() => navigate("/hotel")}
-              className="cursor-pointer col-span-4 sm:col-span-1 inline-flex flex-col items-center border-r border-gray-700 text-black uppercase"
+              className="cursor-pointer  col-span-4 sm:col-span-1 inline-flex flex-col items-center border-r border-gray-700 text-black uppercase"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -299,7 +299,7 @@ const HomeScreen = () => {
 
         <Fade {...props}>
           {sliderData.map((sliderData, index) => (
-            <div className="items-center mt-[87px]" key={index}>
+            <div className="items-center md:mt-22 mt-[87px]" key={index}>
               <div className="relative">
                 <img
                   className="object-cover w-full h-auto rounded shadow-lg"
@@ -320,7 +320,7 @@ const HomeScreen = () => {
             </h2>
           </div>
         </div>
-        <div class="container px-5 py-24 mt-16 mx-auto wrapper">
+        <div class="container px-5 py-24 md:mt-4 mt-16 mx-auto wrapper">
           <div className="grid gap-6 row-gap-5 mb-8 lg:grid-cols-4 sm:row-gap-6 sm:grid-cols-2">
             <a href="/" aria-label="View Item">
               <div className="overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl">
