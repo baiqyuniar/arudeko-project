3ada1ab5f6bde1e3f5ee422f1ed9da4d977a1858
baiqyustiarini <baiqyustiarini@gmail.com>
2022-12-13 08:21:11 +0700

    sidemenu portfolio

--- src/pages/PortfolioPages/LobbyPage.js
+++ src/pages/PortfolioPages/LobbyPage.js
@@ -24,7 +24,7 @@ const LobbyPage = () => {
 
   const handleChange = (e) => {
     setLang(e.target.value);
-    let loc = "http://localhost:3000/tentangkami";
+    let loc = "http://localhost:3000/lobby";
     window.location.replace(loc + "?lng=" + e.target.value);
   };
 
@@ -173,7 +173,202 @@ const LobbyPage = () => {
       </section>
 
       <section id="news" className="w-full pt-32 mb-8">
-        <Lobby />
+        <div className="container mx-auto flex lg:flex-row flex-col">
+          <div className="mt-16 w-full max-w-xl">
+            <ul className="flex flex-col space-y-5">
+              <li className="group w-full block py-3 px-4 border-2 border-gray-100 rounded-md overflow-hidden hover:border-indigo-200">
+                <div className="mx-auto w-full max-w-md">
+                  <a
+                    onClick={(e) => {
+                      e.preventDefault();
+                      navigate("/rumah");
+                    }}
+                    className="flex hover:cursor-pointer flex-col sm:flex-row items-center sm:space-x-10"
+                  >
+                    <span className="flex-shrink-0 inline-block w-2 sm:w-4 h-2 sm:h-4 rounded-full bg-gray-600 bg-opacity-20 sm:bg-opacity-100 group-hover:bg-opacity-100" />
+                    <p className="text-base text-gray-400 font-bold uppercase">
+                      {t("Rumah")}
+                    </p>
+                  </a>
+                </div>
+              </li>
+
+              <li className="group w-full block py-3 px-4 border-2 border-gray-100 rounded-md overflow-hidden hover:border-indigo-200">
+                <div className="mx-auto w-full max-w-md">
+                  <a
+                    onClick={(e) => {
+                      e.preventDefault();
+                      navigate("/hotel");
+                    }}
+                    className="flex hover:cursor-pointer flex-col sm:flex-row items-center sm:space-x-10"
+                  >
+                    <span className="flex-shrink-0 inline-block w-2 sm:w-4 h-2 sm:h-4 rounded-full bg-gray-600 bg-opacity-20 sm:bg-opacity-100 group-hover:bg-opacity-100" />
+                    <p className="text-base text-gray-400 font-bold uppercase">
+                      {t("Hotel")}
+                    </p>
+                  </a>
+                </div>
+              </li>
+
+              <li className="group w-full block py-3 px-4 border-2 border-gray-100 rounded-md overflow-hidden hover:border-indigo-200">
+                <div className="mx-auto w-full max-w-md">
+                  <a
+                    onClick={(e) => {
+                      e.preventDefault();
+                      navigate("/kantor");
+                    }}
+                    className="flex hover:cursor-pointer flex-col sm:flex-row items-center sm:space-x-10"
+                  >
+                    <span className="flex-shrink-0 inline-block w-2 sm:w-4 h-2 sm:h-4 rounded-full bg-gray-600 bg-opacity-20 sm:bg-opacity-100 group-hover:bg-opacity-100" />
+                    <p className="text-base text-gray-400 font-bold uppercase">
+                      {t("Kantor")}
+                    </p>
+                  </a>
+                </div>
+              </li>
+
+              <li className="group w-full block py-3 px-4 border-2 border-gray-100 rounded-md overflow-hidden hover:border-indigo-200">
+                <div className="mx-auto w-full max-w-md">
+                  <a
+                    onClick={(e) => {
+                      e.preventDefault();
+                      navigate("/lobby");
+                    }}
+                    className="flex hover:cursor-pointer flex-col sm:flex-row items-center sm:space-x-10"
+                  >
+                    <span className="flex-shrink-0 inline-block w-2 sm:w-4 h-2 sm:h-4 rounded-full bg-gray-600 bg-opacity-20 sm:bg-opacity-100 group-hover:bg-opacity-100" />
+                    <p className="text-base text-gray-400 font-bold uppercase">
+                      {t("Lobi Apartemen")}
+                    </p>
+                  </a>
+                </div>
+              </li>
+
+              <li className="group w-full block py-3 px-4 border-2 border-gray-100 rounded-md overflow-hidden hover:border-indigo-200">
+                <div className="mx-auto w-full max-w-md">
+                  <a
+                    onClick={(e) => {
+                      e.preventDefault();
+                      navigate("/studio");
+                    }}
+                    className="flex hover:cursor-pointer flex-col sm:flex-row items-center sm:space-x-10"
+                  >
+                    <span className="flex-shrink-0 inline-block w-2 sm:w-4 h-2 sm:h-4 rounded-full bg-gray-600 bg-opacity-20 sm:bg-opacity-100 group-hover:bg-opacity-100" />
+                    <p className="text-base text-gray-400 font-bold uppercase">
+                      {t("Kamar Type Studio")}
+                    </p>
+                  </a>
+                </div>
+              </li>
+
+              <li className="group w-full block py-3 px-4 border-2 border-gray-100 rounded-md overflow-hidden hover:border-indigo-200">
+                <div className="mx-auto w-full max-w-md">
+                  <a
+                    onClick={(e) => {
+                      e.preventDefault();
+                      navigate("/kamar");
+                    }}
+                    className="flex hover:cursor-pointer flex-col sm:flex-row items-center sm:space-x-10"
+                  >
+                    <span className="flex-shrink-0 inline-block w-2 sm:w-4 h-2 sm:h-4 rounded-full bg-gray-600 bg-opacity-20 sm:bg-opacity-100 group-hover:bg-opacity-100" />
+                    <p className="text-base text-gray-400 font-bold uppercase">
+                      {t("Kamar Type 1 BR")}
+                    </p>
+                  </a>
+                </div>
+              </li>
+
+              <li className="group w-full block py-3 px-4 border-2 border-gray-100 rounded-md overflow-hidden hover:border-indigo-200">
+                <div className="mx-auto w-full max-w-md">
+                  <a
+                    onClick={(e) => {
+                      e.preventDefault();
+                      navigate("/coffee");
+                    }}
+                    className="flex hover:cursor-pointer flex-col sm:flex-row items-center sm:space-x-10"
+                  >
+                    <span className="flex-shrink-0 inline-block w-2 sm:w-4 h-2 sm:h-4 rounded-full bg-gray-600 bg-opacity-20 sm:bg-opacity-100 group-hover:bg-opacity-100" />
+                    <p className="text-base text-gray-400 font-bold uppercase">
+                      Coffee Morning
+                    </p>
+                  </a>
+                </div>
+              </li>
+
+              <li className="group w-full block py-3 px-4 border-2 border-gray-100 rounded-md overflow-hidden hover:border-indigo-200">
+                <div className="mx-auto w-full max-w-md">
+                  <a
+                    onClick={(e) => {
+                      e.preventDefault();
+                      navigate("/bedroom");
+                    }}
+                    className="flex hover:cursor-pointer flex-col sm:flex-row items-center sm:space-x-10"
+                  >
+                    <span className="flex-shrink-0 inline-block w-2 sm:w-4 h-2 sm:h-4 rounded-full bg-gray-600 bg-opacity-20 sm:bg-opacity-100 group-hover:bg-opacity-100" />
+                    <p className="text-base text-gray-400 font-bold uppercase">
+                      {t("Kamar Tidur")}
+                    </p>
+                  </a>
+                </div>
+              </li>
+
+              <li className="group w-full block py-3 px-4 border-2 border-gray-100 rounded-md overflow-hidden hover:border-indigo-200">
+                <div className="mx-auto w-full max-w-md">
+                  <a
+                    onClick={(e) => {
+                      e.preventDefault();
+                      navigate("/tamanperumahan");
+                    }}
+                    className="flex hover:cursor-pointer flex-col sm:flex-row items-center sm:space-x-10"
+                  >
+                    <span className="flex-shrink-0 inline-block w-2 sm:w-4 h-2 sm:h-4 rounded-full bg-gray-600 bg-opacity-20 sm:bg-opacity-100 group-hover:bg-opacity-100" />
+                    <p className="text-base text-gray-400 font-bold uppercase">
+                      {t("Landskap Taman Perumahan")}
+                    </p>
+                  </a>
+                </div>
+              </li>
+
+              <li className="group w-full block py-3 px-4 border-2 border-gray-100 rounded-md overflow-hidden hover:border-indigo-200">
+                <div className="mx-auto w-full max-w-md">
+                  <a
+                    onClick={(e) => {
+                      e.preventDefault();
+                      navigate("/tamankecamatan");
+                    }}
+                    className="flex hover:cursor-pointer flex-col sm:flex-row items-center sm:space-x-10"
+                  >
+                    <span className="flex-shrink-0 inline-block w-2 sm:w-4 h-2 sm:h-4 rounded-full bg-gray-600 bg-opacity-20 sm:bg-opacity-100 group-hover:bg-opacity-100" />
+                    <p className="text-base text-gray-400 font-bold uppercase">
+                      {t("Landskap Taman Perumahan")}
+                    </p>
+                  </a>
+                </div>
+              </li>
+
+              <li className="group w-full block py-3 px-4 border-2 border-gray-100 rounded-md overflow-hidden hover:border-indigo-200">
+                <div className="mx-auto w-full max-w-md">
+                  <a
+                    onClick={(e) => {
+                      e.preventDefault();
+                      navigate("/ruangpublik");
+                    }}
+                    className="flex hover:cursor-pointer flex-col sm:flex-row items-center sm:space-x-10"
+                  >
+                    <span className="flex-shrink-0 inline-block w-2 sm:w-4 h-2 sm:h-4 rounded-full bg-gray-600 bg-opacity-20 sm:bg-opacity-100 group-hover:bg-opacity-100" />
+                    <p className="text-base text-gray-400 font-bold uppercase">
+                      {t("Landskap Ruang Publik")}
+                    </p>
+                  </a>
+                </div>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <Lobby />
+          </div>
+        </div>
       </section>
 
       <section id="footer">
